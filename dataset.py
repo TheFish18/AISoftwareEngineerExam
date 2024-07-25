8c54732ca0ed2cea9a95053550818c75f117211a
evana <evana1@umbc.edu>
2024-07-25 14:38:58 -0400

    Creation of the EvenDataset class includes a functioning init with overloaded len and getitem methods. Testing was done to ensure efficient dataset creation. Left in comments to prevent retrials.

--- dataset.py
+++ dataset.py
@@ -29,4 +29,24 @@ class EvenDataset(_IntDataset):
 
         if a or b is not even, raise an error
         """
+        #Prevent invalid data
+        if a % 2 != 0:
+            raise ValueError("a is not an even number. a and b must both be even numbers.")
+        if b % 2 != 0:
+            raise ValueError("b is not an even number. a and b must both be even numbers.")
+        #Initialize data range
+        self.a = a
+        self.b = b
 
+        #Create list of even numbers in the range of a and b
+        self.dataset = list(range(a, b, 2)) #0.1417 secs
+        #self.dataset = list(map(lambda x: x * 2, range(a // 2, b // 2))) #0.8769 secs
+        #self.dataset = [x for x in range(a, b) if x % 2 == 0] #1.2385 secs
+
+    def __len__(self):
+        #Returns the length of the dataset.
+        return len(self.dataset)
+
+    def __getitem__(self, index: int | slice) -> int | List[int]:
+        #Returns the value at index from dataset
+        return self.dataset[index]
