0dd7bf32769cb28dfd74074086d7ab80d57272a1
joshuafisher <joshuafisher@orbisinc.net>
2024-07-25 15:54:15 -0400

    formatting

--- time_dataset.py
+++ time_dataset.py
@@ -7,6 +7,7 @@ def test():
     assert dset[2] == 6, prefix + f"dset[2] should equal 6, got {dset[2]}"
     assert dset[:2] == [2, 4], prefix + f"dset[:2] should equal [2, 4], got {dset[:2]}"
 
+
 def time():
     abs_ = [(2, 2 ** x) for x in range(2, 17)]
 
@@ -22,8 +23,10 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser(description="Test Implementation of EvenDataset")
-    parser.add_argument("--module_name", type=str, default="dataset", help="module name that contains EvenDataset, default 'dataset'")
-    parser.add_argument("--class_name", type=str, default="EvenDataset", help="class name to import, default 'EvenDataset'")
+    parser.add_argument("--module_name", type=str, default="dataset",
+                        help="module name that contains EvenDataset, default 'dataset'")
+    parser.add_argument("--class_name", type=str, default="EvenDataset",
+                        help="class name to import, default 'EvenDataset'")
 
     args = parser.parse_args()
 
@@ -34,10 +37,12 @@ if __name__ == "__main__":
     t = timeit.timeit("time()", setup="from __main__ import time", number=100)
 
     dset = EvenDataset(2, 2 ** 16)
+
+
     def get_size_of(dset):
         return sum(map(sys.getsizeof, dset.__dict__.values()))
+
+
     s = get_size_of(dset)
 
     print(f"time (s): {t}, size (Bytes): {s: ,}")
-
-
